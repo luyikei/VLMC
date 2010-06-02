9ff8b67e00e5eec3877096efe03675c690151131
Rohit Yadav <rohityadav89@gmail.com>
2010-06-02 22:27:44 +0200

    Dpkg, rpmbuild finder cmake modules, updated paths for Mac OS
    Signed-off-by: Hugo Beauzée-Luyssen <beauze.h@gmail.com>

--- cmake/FindLIBVLC.cmake
+++ cmake/FindLIBVLC.cmake
@@ -26,6 +26,9 @@ FIND_PATH(LIBVLC_INCLUDE_DIR vlc/vlc.h
     "/usr/include/vlc"
     "/usr/local/include"
     "/usr/local/include/vlc"
+    #Mac OS
+    "${CMAKE_CURRENT_SOURCE_DIR}/contribs/include"
+    "${CMAKE_CURRENT_SOURCE_DIR}/contribs/include/vlc"
     #mingw
     c:/msys/local/include
   )
@@ -37,6 +40,9 @@ FIND_LIBRARY(LIBVLC_LIBRARY NAMES vlc libvlc
   HINTS "$ENV{LIBVLC_LIBRARY_PATH}"
   PATHS
     "$ENV{LIB_DIR}/lib"
+    #Mac OS
+    "${CMAKE_CURRENT_SOURCE_DIR}/contribs/lib"
+    "${CMAKE_CURRENT_SOURCE_DIR}/contribs/plugins"
     #mingw
     c:/msys/local/lib
   )
@@ -45,6 +51,9 @@ FIND_LIBRARY(LIBVLCCORE_LIBRARY NAMES vlccore libvlccore
   HINTS "$ENV{LIBVLC_LIBRARY_PATH}"
   PATHS
     "$ENV{LIB_DIR}/lib"
+    #Mac OS
+    "${CMAKE_CURRENT_SOURCE_DIR}/contribs/lib"
+    "${CMAKE_CURRENT_SOURCE_DIR}/contribs/plugins"
     #mingw
     c:/msys/local/lib
   )
