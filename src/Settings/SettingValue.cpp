646b43c4345581bc879a5a7ed1c4d477c0656734
Hugo Beauzée-Luyssen <beauze.h@gmail.com>
2010-07-26 11:56:11 +0200

    SettingValue: Handle flags in the SettingValue::set
    This is to ensure the value won't be incoherent, as it was only checked
    in the GUI

--- src/Settings/SettingValue.cpp
+++ src/Settings/SettingValue.cpp
@@ -35,10 +35,20 @@ SettingValue::SettingValue( SettingValue::Type type, const QVariant& defaultValu
 }
 
 void
-SettingValue::set( const QVariant& val )
+SettingValue::set( const QVariant& _val )
 {
+    QVariant val = _val;
     if ( val != m_val )
     {
+        if ( ( m_flags & Clamped ) != 0 )
+        {
+            if ( m_min.isValid() && val.toDouble() < m_min.toDouble() )
+                val = m_min;
+            if ( m_max.isValid() && val.toDouble() > m_max.toDouble() )
+                val = m_max;
+        }
+        if ( ( m_flags & EightMultiple ) != 0 )
+            val = ( val.toInt() + 7 ) & ~7;
         m_val = val;
         emit changed( m_val );
     }
