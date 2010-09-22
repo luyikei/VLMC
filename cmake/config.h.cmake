7c4778065332a9a9479093a05f7c71c034fdc2ac
Rohit Yadav <rohityadav89@gmail.com>
2010-09-23 01:01:14 +0530

    config.h.cmake: Add and use new variables; Hostname, System name, Copyright string etc.

--- cmake/config.h.cmake
+++ cmake/config.h.cmake
@@ -16,8 +16,23 @@
 /* Host computer name */
 #cmakedefine HOSTNAME "@HOSTNAME@"
 
+/* OS Name */
+#cmakedefine SYSNAME "@SYSNAME@"
+
 /* VLMC's version */
 #cmakedefine PROJECT_VERSION "@PROJECT_VERSION@"
+
+/* Version Codename */
+#cmakedefine CODENAME "@CODENAME@"
+
+/* VLMC Copyright string */
+#cmakedefine PROJECT_COPYRIGHT "@PROJECT_COPYRIGHT@"
+
+/* VLMC Contact email */
+#cmakedefine PROJECT_CONTACT "@PROJECT_CONTACT@"
+
+/* VideoLAN website */
+#cmakedefine ORG_WEBSITE "@ORG_WEBSITE@"
 
 /* GUI application ? */
 #cmakedefine WITH_GUI
