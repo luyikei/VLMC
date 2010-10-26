ce992c82b962b34a184dbfa1bedfd6a04419b0d5
Hugo Beauzée-Luyssen <beauze.h@gmail.com>
2010-10-26 16:43:07 +0200

    EffectsListView: Adding forgotten GPL header

--- src/Gui/effectsengine/EffectsListView.cpp
+++ src/Gui/effectsengine/EffectsListView.cpp
@@ -1,3 +1,25 @@
+/*****************************************************************************
+ * EffectsListView.cpp: Display a list of effects
+ *****************************************************************************
+ * Copyright (C) 2008-2010 VideoLAN
+ *
+ * Authors: Hugo Beauzee-Luyssen <beauze.h@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston MA 02110-1301, USA.
+ *****************************************************************************/
+
 #include "EffectsListView.h"
 #include "EffectsEngine.h"
 #include "EffectWidget.h"
@@ -10,7 +32,7 @@
 
 #include <QtDebug>
 
-EffectsListView::EffectsListView(QWidget *parent) :
+EffectsListView::EffectsListView( QWidget *parent ) :
     QListView(parent)
 {
     m_model = new QStandardItemModel( this );
