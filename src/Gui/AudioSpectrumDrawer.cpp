f5fe90adb7aabead40ae5ff69a27db964256aed9
Rohit Yadav <rohityadav89@gmail.com>
2011-06-04 20:14:03 +0530

    AudioSpectrumDrawer: Add missing license block and apply hackings.
    Add missing licence block andcredit to authors from git blame.
    Apply Hackings.

--- src/Gui/AudioSpectrumDrawer.cpp
+++ src/Gui/AudioSpectrumDrawer.cpp
@@ -1,3 +1,25 @@
+/*****************************************************************************
+ * AudioSpectrumDrawer.cpp: Audio Spectrum Drawing Widget
+ *****************************************************************************
+ * Copyright (C) 2008-2011 VideoLAN
+ *
+ * Authors: Geoffroy Lacarriere <geoffroylaca@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston MA 02110-1301, USA.
+ *****************************************************************************/
+
 #include "AudioSpectrumDrawer.h"
 
 AudioSpectrumDrawer::AudioSpectrumDrawer( QPainter* painter, int height, int width, QList<int>* audioValueList )
@@ -7,7 +29,8 @@ AudioSpectrumDrawer::AudioSpectrumDrawer( QPainter* painter, int height, int wid
     m_painter->setPen( QPen( QColor( 79, 106, 25 ), 1, Qt::SolidLine, Qt::FlatCap, Qt::MiterJoin ) );
 }
 
-void    AudioSpectrumDrawer::run()
+void
+AudioSpectrumDrawer::run()
 {
     qreal max = 0;
     for( int i = 0; i < m_audioValueList->count(); i++ )
--- src/Gui/AudioSpectrumDrawer.h
+++ src/Gui/AudioSpectrumDrawer.h
@@ -1,3 +1,25 @@
+/*****************************************************************************
+ * AudioSpectrumDrawer.h: Audio Spectrum Drawing Widget
+ *****************************************************************************
+ * Copyright (C) 2008-2011 VideoLAN
+ *
+ * Authors: Geoffroy Lacarriere <geoffroylaca@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston MA 02110-1301, USA.
+ *****************************************************************************/
+
 #ifndef AUDIOSPECTRUMDRAWER_H
 #define AUDIOSPECTRUMDRAWER_H
 
@@ -7,16 +29,16 @@
 
 class AudioSpectrumDrawer : public QRunnable
 {
+    public:
+        AudioSpectrumDrawer( QPainter* painter, int height, int width, QList<int>* audioValueList );
+        void    run();
+
     private:
         QPainter*       m_painter;
         int             m_height;
         int             m_width;
         QList<int>*     m_audioValueList;
         QPainterPath    m_path;
-
-    public:
-        AudioSpectrumDrawer( QPainter* painter, int height, int width, QList<int>* audioValueList );
-        void    run();
 };
 
 #endif // AUDIOSPECTRUMDRAWER_H
