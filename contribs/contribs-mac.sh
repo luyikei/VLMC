8ff94ea7d82f41b29a761fea4d3e13bf55935e07
Rohit Yadav <rohityadav89@gmail.com>
2012-08-25 03:18:55 +0530

    contribs: update contribs for mac osx
    Build works on Mac OSX 10.8 based on LibVLC 1.1.9

--- contribs/contribs-mac.sh
+++ contribs/contribs-mac.sh
@@ -1,41 +1,36 @@
 #!/bin/sh
 
-set -e
+set -x
+echo "Contribs configured for intel-universal builds"
 
-VLC_ARCH=""
-VLC_BUILD_ID="0109"
-if [ `uname -m` != "i386" ]; then
-    VLC_ARCH="64";
-    VLC_BUILD_ID="0210";
-fi
-echo "Contribs configured for" `uname -m` "architecture"
+LIBVLC_VERSION="1.1.9"
+LIBVLC_VERSION_PREFIX="libvlc-${LIBVLC_VERSION}-macosx"
+LIBVLC_FILE="${LIBVLC_VERSION_PREFIX}.zip"
+LIBVLC_URL="http://bhaisaab.org/contribs/${LIBVLC_FILE}"
 
-VLC_VERSION_DATE="20110719-${VLC_BUILD_ID}"
-VLC_VERSION_PREFIX="1.1-branch-intel${VLC_ARCH}-${VLC_VERSION_DATE}"
-VLC_FILE="${VLC_VERSION_PREFIX}.zip"
-VLC_URL="http://nightlies.videolan.org/build/macosx-intel/${VLC_FILE}"
 FREI0R_FILE="frei0r-plugins-1.2.1.tar.gz"
 FREI0R_URL="http://www.piksel.no/frei0r/releases/frei0r-plugins-1.2.1.tar.gz"
-FREI0R_EFFECTS_FILE="effects${VLC_ARCH}.zip"
-FREI0R_EFFECTS_URL="http://rohityadav.in/files/contribs/effects${VLC_ARCH}.zip"
+FREI0R_EFFECTS_FILE="frei0r-effects-macosx.zip"
+FREI0R_EFFECTS_URL="http://bhaisaab.org/contribs/frei0r-effects-macosx.zip"
+
 QT4_FILE="qt4-4.8-win32-bin.tar.bz2"
-QT4_URL="http://rohityadav.in/files/contribs/qt4-4.8-win32-bin.tar.bz2"
+QT4_URL="http://bhaisaab.org/contribs/${QT4_FILE}"
 
 ROOT_FOLDER=`pwd`
 
 if [ -z `which lrelease` ]; then
     echo "The process require lrelease built in Qt !!!";
-    exit 1;
+#    exit 1;
 fi
 
 # Get the dependencies in this directory
 mkdir -p src-dl/
 cd src-dl/
 
-if [ ! -f $VLC_FILE ]; then
-    curl -C - -O $VLC_URL ;
+if [ ! -f $LIBVLC_FILE ]; then
+    curl -C - -O $LIBVLC_URL ;
 else
-    echo "VLC OK";
+    echo "LIBVLC OK";
 fi
 
 if [ ! -f $FREI0R_FILE ]; then
@@ -60,9 +55,9 @@ cd $ROOT_FOLDER
 mkdir temp
 
 # libvlc
-unzip src-dl/$VLC_FILE "vlc*/VLC.app/Contents/MacOS/*" -d temp
+unzip src-dl/$LIBVLC_FILE "libvlc*" -d temp
 
-cd temp/vlc*/VLC.app/Contents/MacOS/
+cd temp/libvlc-mac/
 mv -fv include/ $ROOT_FOLDER
 mv -fv lib/ $ROOT_FOLDER
 mv -fv plugins/ $ROOT_FOLDER
@@ -71,7 +66,7 @@ cd $ROOT_FOLDER
 
 # frei0r
 tar xvf src-dl/$FREI0R_FILE -C temp --strip-components=2
-cp temp/frei0r.h include
+cp temp/frei0r.h include/
 unzip src-dl/$FREI0R_EFFECTS_FILE
 
 # qt translations
