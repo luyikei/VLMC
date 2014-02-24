87ce86edff1f6937acee2bf2474bce3192eee44c
Hugo Beauzée-Luyssen <hugo@beauzee.fr>
2014-02-24 02:01:50 +0200

    VLCSource: Fix MRL for files

--- src/Backend/VLC/VLCSource.cpp
+++ src/Backend/VLC/VLCSource.cpp
@@ -33,7 +33,7 @@ VLCSource::VLCSource( VLCBackend* backend, const QString& path )
     : m_backend( backend )
     , m_isParsed( false )
 {
-    m_media = new LibVLCpp::Media( backend->vlcInstance(), path );
+    m_media = new LibVLCpp::Media( backend->vlcInstance(), "file://" + path );
 }
 
 VLCSource::~VLCSource()
