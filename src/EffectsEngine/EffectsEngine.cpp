fd7d8fb6e641f51c761284f05f6721c7d5f19fb9
Yikai Lu <luyikei.qmltu@gmail.com>
2016-04-11 15:09:14 +0900

    EffectsEngine: Use QCoreApplication
    Since QApplication requires the "Widgets" module, we should use QCoreApplication
    
    Signed-off-by: Hugo Beauzée-Luyssen <hugo@beauzee.fr>

--- src/EffectsEngine/EffectsEngine.cpp
+++ src/EffectsEngine/EffectsEngine.cpp
@@ -20,7 +20,7 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston MA 02110-1301, USA.
  *****************************************************************************/
 
-#include <QApplication>
+#include <QCoreApplication>
 #include <QDir>
 #include <QProcess>
 #include <QSettings>
