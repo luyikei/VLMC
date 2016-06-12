a8a2ab30d4f05da7c45284511a528ce7bd10716d
Yikai Lu <luyikei.qmltu@gmail.com>
2016-06-12 22:59:52 +0900

    ConsumerEventWatcher

---
+++ src/Tools/ConsumerEventWatcher.h
@@ -0,0 +1,47 @@
+/*****************************************************************************
+ * ConsumerEventWatcher.h: Watches events from a IConsumer and convert them to SIGNAL
+ *****************************************************************************
+ * Copyright (C) 2008-2016 VideoLAN
+ *
+ * Authors: Yikei Lu <luyikei.qmltu@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston MA 02110-1301, USA.
+ *****************************************************************************/
+
+#ifndef CONSUMEREVENTWATCHER_H
+#define CONSUMEREVENTWATCHER_H
+
+
+#include <QObject>
+#include "Backend/IConsumer.h"
+
+class ConsumerEventWatcher : public QObject, public Backend::IConsumerEventCb
+{
+    Q_OBJECT
+public:
+    explicit ConsumerEventWatcher( QObject* parent = 0 );
+
+private:
+    virtual void    onPlaying();
+    virtual void    onStopped();
+    virtual void    onVolumeChanged();
+
+signals:
+    void            playing();
+    void            stopped();
+    void            volumeChanged();
+};
+
+#endif // CONSUMEREVENTWATCHER_H
