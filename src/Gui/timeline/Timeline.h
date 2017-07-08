0a5964ba7af5d0fb1b3f6c0d6e93f19738fd1c4c
Yikai Lu <luyikei.qmltu@gmail.com>
2017-07-09 00:09:22 +0900

    Timeline.cpp: Change authors
    Since I completely rewrote the file, this doesn't represent what it should be.

--- src/Gui/timeline/Timeline.h
+++ src/Gui/timeline/Timeline.h
@@ -3,7 +3,7 @@
  *****************************************************************************
  * Copyright (C) 2008-2016 VideoLAN
  *
- * Authors: Ludovic Fauvet <etix@l0cal.com>
+ * Authors: Yikei Lu    <luyikei.qmltu@gmail.com>
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
